2fadad247c2ec1cbd011c13c33a145f91ba06207
mmervegul <munuremervegul@yahoo.com>
2019-02-01 21:21:41 -0500

    Commit 3. Add logout test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -32,6 +32,18 @@ public class LoginTests {
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
 
+    @Test
+    public void logOutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
+
     @AfterMethod
     public void cleanUp() {
         driver.close();
