fad57294f1580012d7e55b37a79744f2421c60f8
mmervegul <munuremervegul@yahoo.com>
2019-02-08 20:35:19 -0500

    add HashMap for fun

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,19 +4,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.util.Map;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
-// Login tests are maintained here.
-// without page object approach.
 public class LoginTests {
 
     WebDriver driver;
+
+    Map<String, String> myMap = new HashMap<>();
 
     @BeforeMethod
     public void setUp() {
